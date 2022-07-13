754bb08a9186ab86f550da859fbf5760a798853d
vhe1723 <vhe1723@naver.com>
2022-07-13 23:51:22 +0900

    1st Commit Login Logic

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,18 @@
+package hello.login.domain.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private long id;
+
+    @NotEmpty
+    private String loginId; //로그인 Id
+    @NotEmpty
+    private String name;    //사용자 이름
+    @NotEmpty
+    private String password;
+}
